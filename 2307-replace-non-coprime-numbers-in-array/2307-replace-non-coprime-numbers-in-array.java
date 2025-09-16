cceaa7510fc9d6cf6ab9bfd7a8e95549faca7f74
AbhinavChaudhary11 <abhinavchaudhary1103@gmail.com>
2025-09-16 15:40:00 +0530

    Time: 1758 ms (5.15%), Space: 63.6 MB (50.52%) - LeetHub

---
+++ 2307-replace-non-coprime-numbers-in-array/2307-replace-non-coprime-numbers-in-array.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> replaceNonCoprimes(int[] nums) {
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            st.push(nums[i]);
+
+            // ✅ keep merging until top 2 are coprime
+            while (st.size() > 1) {
+                int first = st.pop();
+                int second = st.pop();
+
+                int gcd = getGCD(first, second);
+
+                if (gcd > 1) {
+                    long lcm = (long) first / gcd * second; // prevent overflow
+                    st.push((int) lcm);
+                } else {
+                    st.push(second);
+                    st.push(first);
+                    break;
+                }
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        while (!st.isEmpty()) {
+            ans.add(0, st.pop());
+        }
+        return ans;
+    }
+
+    private int getGCD(int a, int b) {
+        if (b == 0) return a;
+        return getGCD(b, a % b);
+    }
+}
